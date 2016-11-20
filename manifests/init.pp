87b4a54dcd1554762dab56857fde7496a9ad3fe5
jooel nurmi <jooelnurmi@hotmail.fi>
2016-11-20 15:33:34 +0000

    Added solution for php problems (require package apache2)

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,9 @@ class lamp{
 		package {"apache2":}
 		package {"ssh":}
 		package {"php7.0":}
-		package {"libapache2-mod-php":}
+		package {"libapache2-mod-php":
+			require => Package ["apache2"],
+	}
 		package {"mysql-client":}
 		package {"mysql-server":}
 	
